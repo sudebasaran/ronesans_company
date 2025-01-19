96aec93b9b2de553e2b75ca546a02555370238c8
sudebasaran <sudebasarnn@gmail.com>
2025-01-19 13:13:38 +0300

    ronesans_company

---
+++ ronesans/app.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
+
+
+
+
+
